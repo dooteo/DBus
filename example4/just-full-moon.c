ca4389fa6766fa31bbbdf8e498fe7c9125a204ea
dooteo <dooteo@macpro.zundan.com>
2016-10-19 21:11:33 +0200

    Add exaple 4: DBus Services

---
+++ example4/just-full-moon.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <dbus/dbus.h>
+
+int main() {
+
+	DBusConnection *connection;
+	DBusError error;
+
+	dbus_error_init(&error);
+	connection = dbus_bus_get(DBUS_BUS_STARTER, &error); /* DBUS_BUS_STARTER is
+														the bus that started us */
+
+	/* Do something here to make sure that the application was successfully
+	started by DBus
+	* Example could be something like 
+	*/
+	FILE *tmp;
+	tmp = fopen("/tmp/just_full_moon.result", "w");
+
+	//if ( dbus_message_is_signal(message,"org.share.linux","StartServiceByName") ) {
+		fprintf(tmp,"'Just full moon' service was started successfully");	
+/*	} else {
+		fprintf(tmp,"share-linux service was started with unknown command");	
+	}
+*/
+	fclose(tmp);
+	
+
+	/* After that you have the service up, so you can do whetever you like */
+	dbus_connection_unref(connection);
+
+	return 0;
+}
---
+++ example4/start-service.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <dbus/dbus.h>
+
+int main() {
+
+	DBusConnection *connection;
+	DBusError error;
+	DBusMessage *message;
+
+	const char *service_name = "just.full.moon";
+	dbus_uint32_t flag; /* Currently this is not used by DBus, they say it is for
+						futur expansion*/
+
+	dbus_bool_t result;
+	dbus_error_init(&error);
+
+	connection = dbus_bus_get(DBUS_BUS_SESSION, &error);
+
+	if ( dbus_error_is_set(&error) ) {
+
+		printf("Error getting dbus connection: %s\n",error.message);
+		dbus_error_free(&error);
+		dbus_connection_unref(connection);
+		return 0;
+	}
+
+	message = dbus_message_new_method_call("org.freedesktop.DBus",
+							"/org/freedesktop/DBus",
+							"org.freedesktop.DBus",
+							"StartServiceByName");
+
+/*
+	message = dbus_message_new_method_call("just.full.moon",
+							"/just/full/moon",
+							"just.full.moon",
+							"StartServiceByName");
+
+*/
+	if ( !message ) {
+
+		printf("Error creating DBus message\n");
+		dbus_connection_unref(connection);
+		return 0;
+	}
+
+	dbus_message_set_no_reply(message, TRUE); // We don't want to receive a reply
+
+	/* Append the argument to the message, must ends with DBUS_TYPE_UINT32 */
+	dbus_message_append_args(message, DBUS_TYPE_STRING, &service_name, 
+							DBUS_TYPE_UINT32, &flag, DBUS_TYPE_INVALID);
+
+	result = dbus_connection_send(connection, message, NULL);
+
+	if ( result == TRUE ) {
+
+		printf("Successfully activating the %s service\n",service_name);
+
+	} else {
+
+		printf("Failed to activate the %s service\n",service_name);
+
+	}
+
+	dbus_message_unref(message);
+	dbus_connection_unref(connection);
+
+	return 0;
+}
+
